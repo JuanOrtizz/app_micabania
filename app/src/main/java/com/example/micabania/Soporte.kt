ec6efe63daf85f016dd21311dca16d5769a7edf6
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-05-20 15:07:15 -0300

    feat: Se agregan funcionalidades para enviar SMS Y Mails a los contactos de la empresa para soporte

--- app/src/main/java/com/example/micabania/Soporte.kt
+++ app/src/main/java/com/example/micabania/Soporte.kt
@@ -1,6 +1,9 @@
 package com.example.micabania
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +14,29 @@ class Soporte : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_soporte)
+
+        val botonSms = findViewById<Button>(R.id.botonContactoSms)
+        val botonEmail = findViewById<Button>(R.id.botonContactoMail)
+
+        // Listener para el boton contacto via sms
+        botonSms.setOnClickListener {
+            val numero = "" // <-- numero de soporte de la app
+            val mensaje = "Hola, necesito ayuda con la aplicación" // mensaje predefinido
+
+            val intent = Intent(Intent.ACTION_SENDTO).apply { // indica que abra la app para sms y cree un mensaje para el numero de soporte con un texto predefinido
+                data = Uri.parse("smsto:$numero") // numero al que manda el email
+                putExtra("sms_body", mensaje) // mensaje predefinido
+            }
+            startActivity(intent) //inicia la app de SMS
+        }
+
+        // Listener para el boton contacto via Email
+        botonEmail.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SENDTO).apply { // indica que abra la app para emails y cree un correo para la direccion de soporte con un asunto predefinido
+                data = Uri.parse("mailto:jaja@gmail.com") // direccion de soporte
+                putExtra(Intent.EXTRA_SUBJECT, "Consulta sobre la app") // asunto del mail
+            }
+            startActivity(intent)// inicia la app para enviar el email
+        }
     }
 }
