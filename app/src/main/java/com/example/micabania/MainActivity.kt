1b88f7ef34912cefd196571a7455db5310a97933
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-05-20 13:48:23 -0300

    feat: Se cambiaron ET por Inputs de Material Desing y se realizaron validaciones en el front

--- app/src/main/java/com/example/micabania/MainActivity.kt
+++ app/src/main/java/com/example/micabania/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.micabania
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,15 @@ class MainActivity : AppCompatActivity() {
         botonIniciarSesion.setOnClickListener(){
             val intent = Intent(this, MenuPrincipal::class.java)
             startActivity(intent)
+        }
+
+        // Mostrar Snackbar si el usuario se registra con exito.
+        val contextView = findViewById<View>(R.id.vista_main)
+        // recibe el mensaje por el intent que mandamos de activity CreaTuCuenta
+        val mensaje = intent.getStringExtra("mensaje_snackbar")
+        // Si el mensaje no esta vacio crea el snackbar
+        if (!mensaje.isNullOrEmpty()) {
+            Snackbar.make(contextView, mensaje, Snackbar.LENGTH_LONG).show()
         }
     }
 }
