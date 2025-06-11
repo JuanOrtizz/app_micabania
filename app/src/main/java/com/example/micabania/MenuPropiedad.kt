e32461ea8623a90f181369bfb0cb7839f0feb262
JuanOrtizz <juan_ignaortiz@outlook.com>
2025-06-11 01:51:31 -0300

    feat: Actualizacion de Activity Menu Propiedad (XML y KT)

--- app/src/main/java/com/example/micabania/MenuPropiedad.kt
+++ app/src/main/java/com/example/micabania/MenuPropiedad.kt
@@ -1,32 +1,83 @@
 package com.example.micabania
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MenuPropiedad : AppCompatActivity() {
+    // Declaro variables
+    private lateinit var btnVolver:ImageButton
+    private lateinit var btnCategoriaCocina: Button
+    private lateinit var btnCategoriaBlanqueria: Button
+    private lateinit var btnCategoriaBaño: Button
+    private lateinit var btnCategoriaLimpieza: Button
+    private lateinit var btnCategoriaExteriores: Button
+    private lateinit var btnEliminar: Button
+
+    // Funcion OnCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu_propiedad)
 
-        /*intent para el boton volver*/
-        val botonVolver = findViewById<ImageButton>(R.id.botonVolver)
-        botonVolver.setOnClickListener(){
-            val intent = Intent(this, MenuPrincipal::class.java)
-            startActivity(intent)
+        // Inicializo las variables al cargar la activity
+        btnVolver = findViewById(R.id.botonVolver)
+        btnCategoriaCocina = findViewById(R.id.botonCategoriaCocina)
+        btnCategoriaBlanqueria = findViewById(R.id.botonCategoriaBlanqueria)
+        btnCategoriaBaño = findViewById(R.id.botonCategoriaBaño)
+        btnCategoriaLimpieza = findViewById(R.id.botonCategoriaLimpieza)
+        btnCategoriaExteriores = findViewById(R.id.botonCategoriaExteriores)
+        btnEliminar = findViewById(R.id.botonEliminarPropiedad)
+
+        /*Listener para el boton volver*/
+        btnVolver.setOnClickListener{
+            finish()
         }
 
-        /*intent para el boton categoria Cocina*/
-        val botonCategoriaCocina = findViewById<Button>(R.id.botonCategoriaCocina)
-        botonCategoriaCocina.setOnClickListener(){
-            val intent = Intent(this, CategoriaCocina::class.java)
-            startActivity(intent)
+        /*Listener para el boton categoria Cocina*/
+        btnCategoriaCocina.setOnClickListener{
+            cargarActivityCategoria(R.drawable.icon_cocina, "Cocina")
         }
+
+        btnCategoriaBlanqueria.setOnClickListener{
+            cargarActivityCategoria(R.drawable.icon_blanqueria, "Blanqueria")
+        }
+
+        btnCategoriaBaño.setOnClickListener{
+            cargarActivityCategoria(R.drawable.icon_banio, "Baño")
+        }
+
+        btnCategoriaLimpieza.setOnClickListener{
+            cargarActivityCategoria(R.drawable.icon_limpieza, "Limpieza")
+        }
+
+        btnCategoriaExteriores.setOnClickListener{
+            cargarActivityCategoria(R.drawable.icon_exteriores, "Exteriores y Otros")
+        }
+
+        btnEliminar.setOnClickListener{
+            DialogConfirmacion(
+               onConfirmar = {
+                   //Cuando conecte con la DB, lo va a eliminar de la misma.
+                   val resultIntent = Intent().apply {
+                       putExtra("mensaje_snackbar", "Eliminaste la propiedad") //devuelve el mensaje para el snackbar
+                   }
+                   //Metodo setResult para enviar los datos.
+                   setResult(Activity.RESULT_OK, resultIntent)
+                   finish()// finalizo esta activity asi gestiono la memoria
+                }
+            ).show(supportFragmentManager, "ConfirmDialog")
+        }
+
     }
+
+    private fun cargarActivityCategoria(imagenId:Int, textoCategoria:String){
+        val intent = Intent(this, Categoria::class.java)
+        intent.putExtra("imagenId", imagenId)
+        intent.putExtra("categoria", textoCategoria)
+        startActivity(intent)
+    }
+
 }
